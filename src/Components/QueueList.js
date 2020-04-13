c6da6a5f253cf5cfd4bf5e0df72e322fa7552331
Ashrit Koirala <ashritboi@gmail.com>
2020-04-13 16:09:56 -0500

    not finished

--- src/Components/QueueList.js
+++ src/Components/QueueList.js
@@ -4,76 +4,142 @@ import { db } from "../firebase";
 import { Button, Card, Row, Col } from "react-bootstrap";
 import firebase from "firebase/app";
 import Headerh from "./Layouts/Header";
-import React, { Component } from "react";
+import React, { Component, Children } from "react";
 import * as routes from "../constants/routes";
 import { KeyboardAwareScrollView } from "react";
 import { Appbar } from "react";
-// import Search from "./src/components/Search";//!< Imports search component in this screen
 import { StyleSheet, Text, View, TextInput } from "react"; //!< Import various designing tools from react-native mainly for Styling
 
 
 class QueueList extends Component {
    state = {
-    Queue:null ,
-    
-  };
-  ondelete = () => {
-    const { history } = this.props;
-    this.refs.remove();
-    history.push(routes.HOME);
-    
-  };
+    Queue:[] ,
+   }
+    ondelete = (key) => {
+      const { history } = this.props; 
+          var Q= firebase.database().ref("Queue");
+         Q.child(key).name.remove(); 
+         
+  
+      console.log("this is the keuy ---------------------------"+ key);
+      }; 
   componentDidMount() {
-
    db.doGetQueue().then(snapshot =>
       this.setState(() => ({ Queue: snapshot.val() }))
     );
   }
 
-  // Object.keys(Queue).map(key =>
-  //   <div key={key}>{Queue[key].name}  {Queue[key].Advisor} {Queue[key].message} {Queue[key].Student_id}
-  //   {/* <Button variant="outline-danger"
-  //   onClick={this.ondelete}
-  //   >
-  //     Delete
-  //     </Button> */}
-  //   </div>
-  // )
-  
   render() {
-
     const { Queue } = this.state;
     return (
       <div className='container'>
   <section className='display-item'>
     <div className="wrapper">
-        
-    { !!Queue && Object.keys(Queue).map(key =>
+    <h2>List of Students on Advising Queue</h2>
+
+    { Object.keys(Queue).map(key =>
     <div key={key}>{Queue[key].name}  {Queue[key].Advisor} {Queue[key].message} {Queue[key].Student_id}
-  <Button variant="outline-danger"
-     onClick={this.ondelete}
-     >
-       Delete
-       </Button>
-    </div>
+      <Button variant="outline-danger" onClick={this.ondelete( key)} > Delete</Button>
+      </div>
   )
      }
-
     </div>
   </section>
 </div>
-    );
-    }
+    );}
 }
 
 
-const UserList = ({ Queue }) =>
-  <div>
-    <h2>List of Students on Advising Queue</h2>
-
-    
-  </div>
-
 const authCondition = authUser => !!authUser;
-
 export default withAuthorization(authCondition)(QueueList); //grants authorization to open endpoint if an user is signed in
+
+
+
+
+// import withAuthorization from "./withAuthorization";
+// import { db } from "../firebase";
+// import { Button, Card, Row, Col } from "react-bootstrap";
+// import firebase from "firebase/app";
+// import Headerh from "./Layouts/Header";
+// import React, { Component, Children, FlatList } from "react";
+// import * as routes from "../constants/routes";
+// import { KeyboardAwareScrollView } from "react";
+// import { Appbar } from "react";
+// // import Search from "./src/components/Search";//!< Imports search component in this screen
+// import { StyleSheet, Text, View, TextInput } from "react"; //!< Import various designing tools from react-native mainly for Styling
+
+
+//  class QueueList extends Component {
+   
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//         q_list: [],
+//         uid:'',
+//     }
+// }
+// updatelist=q_list=>{
+//   this.setState({q_list:[...this.state.q_list, q_list]});
+// }
+
+// ondelete = (itemid)=>{
+//   var itemref = firebase.database().ref("Queue");
+//       itemref.child(itemid).remove();     
+//       }
+  
+// iteminfo=itemid=>{
+//   var itemref = firebase.database().ref("Queue");
+//   itemref.once("value", data=>{
+//       const name= data.child(itemid).val().name;      
+//   })
+// }
+// componentDidMount(){
+//   var self = this;
+//     var user = firebase.auth().currentUser;
+//     var uid;
+    
+  
+//   var itemref = firebase.database().ref("Queue");
+//   itemref.once("value",dataSnapShot=>{
+//       if(dataSnapShot.val()){
+//           let dobobj = Object.values(dataSnapShot.val());
+//           for (var i = 0; i < dataSnapShot.numChildren(); i++) {
+//               if(dobobj[i].uid == uid){
+//               self.updatelist(dobobj[i]);
+//               }
+//           }
+          
+//       }
+//   })
+// }
+// render(){
+//   return(
+//           <View >
+//           <View >
+//               <Text >Item(s) you have</Text>
+//           </View>
+//           <FlatList
+//           data={this.state.q_list}
+         
+//           inverted
+//           keyExtractor={(item, index)=> index.toString()}
+//           renderItem={({item})=>(
+//               <Card style={{backgroundColor:'#393636'}} >
+//                   <View >
+//                   <Text >{"Item Name: " +item.name }</Text>
+//                   </View>
+              
+//           </Card> 
+//           )}
+//           ></FlatList>
+//           </View>
+//   )
+// }
+//  }
+
+
+// const authCondition = authUser => !!authUser;
+
+// export default withAuthorization(authCondition)(QueueList); //grants authorization to open endpoint if an user is signed in
+
+
